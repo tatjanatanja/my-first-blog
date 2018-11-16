a51cbf7ca8b07640bde185ec9a114a08a9cfd6b2
tanja <tanjanewwision@gmail.com>
2018-11-16 15:41:03 +0100

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+"""mysite URL Configuration
+
+[...]
+"""
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	path('', include('blog.urls')),
+]
